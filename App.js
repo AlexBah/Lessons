918deeac30472916e6f85af0340ccb6a2e01f5f4
Alex Bah <P_osts@mail.ru>
2024-01-31 23:30:54 +0700

    Lesson1

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image, TouchableWithoutFeedback} from 'react-native';
 
 export default function App() {
+  const handleTextPress = () => console.log('text pressed');
+  const handleButtonPress = () => Alert.alert('Name of window', 'Main message', [
+    {text: 'yes', onPress: () => console.log('Yes button')},
+    {text: 'cancel', onPress: () => console.log('Cancel button')}
+  ]);
+
   return (
+// {'/n'} перевод строки
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text></Text>
+      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}>Hello!!</Text>
+      <Button title='Нажми на меня' color='red' onPress={handleButtonPress}/>
+      <TouchableWithoutFeedback onPress={handleButtonPress}>
+        <Image blurRadius={1} source={{
+          width: 200,
+          height: 150,
+          uri: "https://avatars.githubusercontent.com/u/37043482"
+        }}/>
+      </TouchableWithoutFeedback>
+    <StatusBar style="auto" /> 
     </View>
   );
 }
@@ -14,7 +30,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
   },
 });
