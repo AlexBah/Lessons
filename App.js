1227619a18caaf36b3d614b0e30dae4e037f3a36
Alex Bah <P_osts@mail.ru>
2024-04-07 13:24:52 +0700

    guides1

--- App.js
+++ App.js
@@ -1,45 +1,17 @@
-import React, { useState } from 'react';
-import { FlatList, StyleSheet, View, Text} from 'react-native';
-import Header from './components/Header'
-import ListItem from './components/ListItem'
-import Form from './components/Form'
+import React from 'react';
+import {View, Text} from 'react-native';
 
-export default function App() {
-  const [listOfItems, setListOfItems] = useState( [
-    {text: 'Купить молоко', key: '1'},
-    {text: 'Помыть машину', key: '2'},
-    {text: 'Купить картошку', key: '3'},
-    {text: 'Стать миллионером', key: '4'}
-  ] )
-
-  const addHandler= (text) => {
-    setListOfItems((list) => {
-      return [
-        { text: text, key: Math.random().toString(36).substring(7) },
-        ...list
-      ]
-    })
-  }
-
-  const deleteHandler = (key) => {
-    setListOfItems((list) => {
-      return list.filter(listOfItems => listOfItems.key != key)
-    });
-  }
-
+const YourApp = () => {
   return (
-    <View>
-      <Header />
-      <Form addHandler={addHandler}/>
-      <View>
-        <FlatList data={listOfItems} renderItem={({ item }) => (
-          <ListItem el={item} deleteHandler={deleteHandler} />
-        )}/>
-      </View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Try editing me!</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-
-});
+export default YourApp;
